5de82808e4568632101de6686ac55aa865393c37
zhq <zhq199622@163.com>
2020-04-06 20:11:17 +0800

    初步实现 add todo

--- index.js
+++ index.js
@@ -5,14 +5,29 @@ const p=require('path');
 const dbPath=p.join(home,'.todo');
 
 module.exports = {
-    add: (...name) => {
+    add: (...args) => {
         // 读取已有任务
         // 添加新任务
         // 存储任务到文件
-        console.log(name[1].join(" "));
+        const title=args[1].join(" ");
         fs.readFile(dbPath,{flag:'a+'}, (err, data) => {
             if (err) throw err;
-            console.log(data);
+            let list;
+            try {
+                list=JSON.parse(data.toString());
+            }catch (error2) {
+                list=[] // list 的默认值
+            }
+            const task={
+                title,
+                done:false
+            }
+            list.push(task)
+            const result=JSON.stringify(list)
+            fs.writeFile(dbPath, result, (err) => {
+                if (err) throw err;
+                console.log('The file has been saved!');
+            });
         })
     }
 }
